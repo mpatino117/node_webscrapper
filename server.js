1d1ed0aa5f37a05267d9d3b701019bb6248bdca9
mpatino117 <mpatino117@gmail.com>
2017-02-13 17:17:14 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
+const app = express();
+
+app.get('/scrape', ()=> {
+  url = 'http://www.imdb.com/title/tt1229340/'
+
+
+}
+
+app.listen('8000');
+
+exports = module.exports = app;
